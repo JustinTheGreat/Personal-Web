c61e4d04a5e2a69d0ac7167c10a7e753819075cc
JustinTheGreat <marcusgong123@gmail.com>
2023-06-10 19:58:35 -0400

    New Update

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const logo = document.getElementById('logo');
-
-logo.addEventListener('mouseover', () => {
-  logo.src = 'images/Logo.jpg';
+//Tab Change effect
+let docTitle = document.title;
+window.addEventListener("blur",() => {
+  document.title = "Come Back ;(";
 });
 
-logo.addEventListener('mouseout', () => {
-  logo.src = 'images/Logo.gif';
+window.addEventListener("focus",() => {
+  document.title = docTitle;
+});
+//Scrolling effect to go down
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.scroll-button').addEventListener('click', function(event) {
+    event.preventDefault();  // Prevent the default action
+    window.scrollBy({ top: 600, left: 0, behavior: 'smooth' }); // You can change '600' to the amount you want to scroll
+  });
 });
